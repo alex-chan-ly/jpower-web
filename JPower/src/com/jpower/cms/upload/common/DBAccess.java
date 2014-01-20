a1967c82bf6408ba4a1996e33f3b5d6de13d9172
alex.chan.ly@gmail.com <alex.chan.ly@gmail.com@82f40284-075e-f0e9-6470-bbb5d8a25014>
2014-01-20 15:26:43 +0000

    Enhance DBAccess with DB connection Pool

--- JPower/src/com/jpower/cms/upload/common/DBAccess.java
+++ JPower/src/com/jpower/cms/upload/common/DBAccess.java
@@ -4,58 +4,64 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.apache.derby.jdbc.EmbeddedConnectionPoolDataSource;
+
+import biz.source_code.miniConnectionPoolManager.MiniConnectionPoolManager;
+
 public class DBAccess {
 	
-	public static String driver = "org.apache.derby.jdbc.EmbeddedDriver";
-//	public static String dbName="jPowerDB";
-//	public static String dbName="db1";
-	public static String dbName="/home/benson/Project/JPower/content/db/db1";
-	public static String connectionURL = "jdbc:derby:" + dbName + ";create=true";
-	public static Connection conn = null;
-	
-	private static Connection acquireDBConnection() {
-		try {
-		
-			Class.forName(driver);
-			
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-		try {
-			conn = DriverManager.getConnection(connectionURL);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		return conn;
-	}
-	
-	public static Connection getDBConnection() {
-		if (conn == null) {
-			return acquireDBConnection();
-		} else {
-			return conn;
-		}
-	}
-	
-	public static void shutdownDB() {
-		boolean gotSQLExc = false;
+		private static MiniConnectionPoolManager poolMgr = null;
+//		private static String dataBaseName = "db2";
+		private static String dataBaseName = "db1";
+//        public static String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+//        public static String dbName="db1";
+//        public static String connectionURL = "jdbc:derby:" + dbName + ";create=true";
+       
+        public static Connection conn = null;
+       
+        private static Connection acquireDBConnection() {
+        	Connection conn = null;
+    		EmbeddedConnectionPoolDataSource dataSource = new EmbeddedConnectionPoolDataSource();
+    		dataSource.setDatabaseName(dataBaseName);
+    		poolMgr = new MiniConnectionPoolManager(dataSource, Util.dbPoolSize);
+            
+    		try {
+				conn =  poolMgr.getConnection();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+    		return conn;
+        }
+       
+        public static Connection getDBConnection() {
+        	Connection conn = null;
+        	try {
+                if (poolMgr == null) {
+                    conn = acquireDBConnection();
+            } else {
+                    conn = poolMgr.getConnection();
+            }        		
+        	} catch (SQLException e) {
+        		e.printStackTrace();
+        	}
+        	
+        	return conn;
+        }
+       
+        public static void shutdownDB() {
+                boolean gotSQLExc = false;
         try {
            DriverManager.getConnection("jdbc:derby:;shutdown=true");
-        } catch (SQLException se)  {	
-           if ( se.getSQLState().equals("XJ015") ) {		
+        } catch (SQLException se)  {    
+           if ( se.getSQLState().equals("XJ015") ) {            
               gotSQLExc = true;
            }
         }
         if (!gotSQLExc) {
-        	System.out.println("Database did not shut down normally");
+                System.out.println("Database did not shut down normally");
         }  else  {
-           System.out.println("Database shut down normally");	
+           System.out.println("Database shut down normally");  
         }  
-	}
-	
-	public static void returnDBConnection(Connection conn) {
-		
-	}
+        }
 }
+
